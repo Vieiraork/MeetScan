682b99e50b5a0961c585394c2367e68bba9b9093
William Vieira <53006695+Vieiraork@users.noreply.github.com>
2023-10-30 16:59:53 -0300

    Change - adicionando código do RFID

--- webserver/webserver.ino
+++ webserver/webserver.ino
@@ -1,14 +1,22 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <MFRC522.h>
 
 
-const char* rede  = "";
-const char* senha = "";
+const char* rede        = "";
+const char* senha       = "";
+const uint8_t RESET_PIN = D3;
+const uint8_t SS_PIN    = D4;
 
 IPAddress ip(192, 168, 100, 2);
 IPAddress gateway(192, 168, 100, 1);
 IPAddress subnet(255, 255, 255, 0);
-IPAddress dns(192, 168, 100, 1);   
+IPAddress dns(192, 168, 100, 1);
+
+MFRC522 rfid(SS_PIN, RESET_PIN);
+MRFC522::MIFARE_Key key;
+
+String tag;
 
 ESP8266WebServer server(8000);
 int contador = 0;
@@ -55,6 +63,9 @@ void setup() {
     imprimeMensagemConexao();
   }
 
+  SPI.begin();
+  rfid.PCD_Init();
+
   Serial.println(WiFi.localIP());
 
   server.on("/", enviaResposta);
@@ -63,6 +74,25 @@ void setup() {
   Serial.println("Servidor HTTP foi iniciado.");
 }
 
+void sendTagForServer(String tag) {
+  // TODO - IMPLEMENTAR A CHAMADA DO SERVIDOR
+}
+
 void loop() {
   server.handleClient();
+
+  if (!rfid.PICC_IsNewCardPresent())
+    return;
+  if (rfid.PICC_ReadCardSerial()) {
+    for (byte i=0; i < 4; i++) {
+      tag += rfid.uid.uidByte[i];
+    }
+    // Chamar servidor laravel
+    Serial.println(tag);
+    sendTagForServer(tag);
+    tag = "";
+    rfid.PICC_HaltA();
+    rfid.PCD_StopCrypto1();
+  }
+  }
 }
