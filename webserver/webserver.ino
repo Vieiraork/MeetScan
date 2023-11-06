009c141e7007428e9b9ad67a7213b2e5e8df80d0
William Vieira <53006695+Vieiraork@users.noreply.github.com>
2023-11-05 23:39:10 -0300

    Change - alterando código para notificação via rfid

--- webserver/webserver.ino
+++ webserver/webserver.ino
@@ -1,98 +1,87 @@
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
 #include <MFRC522.h>
+#include <SPI.h>
 
+const char* rede      = "";
+const char* senha     = "";
+const uint8_t RST_PIN = D3;
+const uint8_t SS_PIN  = D4; 
 
-const char* rede        = "";
-const char* senha       = "";
-const uint8_t RESET_PIN = D3;
-const uint8_t SS_PIN    = D4;
-
-IPAddress ip(192, 168, 100, 2);
-IPAddress gateway(192, 168, 100, 1);
-IPAddress subnet(255, 255, 255, 0);
-IPAddress dns(192, 168, 100, 1);
-
-MFRC522 rfid(SS_PIN, RESET_PIN);
-MRFC522::MIFARE_Key key;
+ESP8266WiFiMulti WiFiMulti;
+MFRC522 rfid(SS_PIN, RST_PIN);
+MFRC522::MIFARE_Key key;
 
 String tag;
 
-ESP8266WebServer server(8000);
-int contador = 0;
-
-void enviaResposta() {
-  server.send(200, "text/plain", "hello from ESP8266");
-}
-
-void imprimeMensagemConexao() {
-  if (contador == 0){
-    Serial.println("Conectando no WIFI.");
-    Serial.flush(); 
-  }
-  else if (contador == 1){
-    Serial.println("Conectando no WIFI..");
-    Serial.flush();
-  }
-  else if (contador == 2){
-    Serial.println("Conectando no WIFI...");
-    Serial.flush();
-  }
-
-  contador++;
-  if (contador == 3) {
-    contador=0;
-  }
-}
-
-void capturaParametro() {
-  String paramNome = server.arg(0);
-
-  String mensagem = "Parâmetro: " + paramNome;
-  server.send(200, "text/plain", mensagem);
-}
-
 void setup() {
+  int loadDot = 0;
   Serial.begin(115200);
-
-  WiFi.config(ip, gateway, subnet, dns);
-  WiFi.begin(rede, senha);
-
-  while(WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    imprimeMensagemConexao();
-  }
-
   SPI.begin();
   rfid.PCD_Init();
 
+  // Serial.setDebugOutput(true);
+
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+  for (uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.flush();
+    delay(1000);
+  }
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(rede, senha);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    
+    loadDot++;
+  }
+  Serial.print("");
+  Serial.print("Ip local: ");
   Serial.println(WiFi.localIP());
-
-  server.on("/", enviaResposta);
-  server.on("/param", capturaParametro);
-  server.begin();
-  Serial.println("Servidor HTTP foi iniciado.");
-}
-
-void sendTagForServer(String tag) {
-  // TODO - IMPLEMENTAR A CHAMADA DO SERVIDOR
 }
 
 void loop() {
-  server.handleClient();
+    WiFiClient client;
 
-  if (!rfid.PICC_IsNewCardPresent())
-    return;
-  if (rfid.PICC_ReadCardSerial()) {
-    for (byte i=0; i < 4; i++) {
-      tag += rfid.uid.uidByte[i];
+    HTTPClient http;
+    if (!rfid.PICC_IsNewCardPresent())
+      return;
+    
+    if (rfid.PICC_ReadCardSerial()) {
+      for (byte i = 0; i < 4; i++) {
+        tag += rfid.uid.uidByte[i];
+      }
     }
-    // Chamar servidor laravel
-    Serial.println(tag);
-    sendTagForServer(tag);
-    tag = "";
-    rfid.PICC_HaltA();
-    rfid.PCD_StopCrypto1();
-  }
-  }
+
+    if (tag != "") {
+      if (http.begin(client, "http://192.168.100.7/meetscan-app/public/api/codigos/" + tag)) {
+        int code = http.GET();
+        String res = http.getString();
+
+        if (code > 0) {
+          Serial.print("Http code: ");
+          Serial.println(code);
+          Serial.print("Response: ");
+          Serial.println(res);
+        } else {
+          Serial.print("Error: ");
+          Serial.println(res);
+        }
+
+        Serial.println(tag);
+        tag = "";
+        rfid.PICC_HaltA();
+        rfid.PCD_StopCrypto1();
+      }
+    }
+
+    delay(5000);
 }
